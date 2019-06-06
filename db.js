e1f45c7239f3034a6104df307e2dc5eeedba57ce
luzhang1311 <zlsx@mail.ustc.edu.cn>
2019-06-06 23:17:25 +0200

    send to backend2

--- db.js
+++ db.js
@@ -1,18 +1,26 @@
+const storage = require('firebase/storage');
 const firebase = require('firebase/app');
 const firestore = require('firebase/firestore');
+global.XMLHttpRequest = require("xhr2");
+const noop = ()=>{};
 
 const firebaseConfig = {
   apiKey: "AIzaSyCxvZl2ki_Zetmkd-F-5R5PSKlY7Q-VFAE",
   authDomain: "project-path4.firebaseapp.com",
   databaseURL: "https://project-path4.firebaseio.com",
   projectId: "project-path4",
-  storageBucket: "project-path4.appspot.com",
+  // storageBucket: "project-path4.appspot.com",
+  storageBucket:"images-path4",
   messagingSenderId: "574572197316",
   appId: "1:574572197316:web:315283222621ab55"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
+noop(firestore)
+noop(storage)
 
-module.exports = db;
+const db = firebase.firestore();
+const files = firebase.storage();
+
+module.exports = {db, files};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require('cors');
-const db = require('./db.js')
+const fetch = require('node-fetch');
+const db = require('./db.js').db
+const storage = require('./db.js').files
 const app = express();
-
-const port = 8080;
+const port = 8084;
 
 app.use(cors());
 
@@ -25,6 +26,61 @@ app.get('/random', function (req, res) {
     });
 });
 
+// Promise error catch function
+const catcher = (e) => {
+  console.log('I have  this error in a Promise:',e)
+}
+
 app.listen(port, () => {
  console.log("The API is running on port " + port);
 });
+
+// setInterval(function(){ console.log("hi")},1000);
+
+// Every 20 secs fetch the new image
+setInterval(saveImgtoGcloud,20000);
+
+function saveImgtoGcloud(){
+
+  // Camera URL
+  const url = 'https://us-central1-core-228912.cloudfunctions.net/raspberry-pi-a7?apiKey=MFDW!!HQ_!CCPK?Q?QJEGAKQ!ENFM%3C%3E(XGQ}BXAMM%3CQ:'
+  
+  // Create a root reference
+  var storageRef = storage.ref();
+  var date = new Date(); 
+  var uid =  'img'+date.getTime() +'.jpeg'
+
+  // Create a reference to the created uid(location of bucket in firestore)
+  var ref = storageRef.child(uid);
+  fetch(url)
+  .then(res => res.arrayBuffer())
+  // Gets the response and returns it as a arrayBuffer()
+
+  .then(blob=> {
+    // Here's where you get access to the blob
+    // And you can use it for whatever you want
+    // Like calling ref().put(blob)
+    var aa = new Uint8Array(blob)
+    const urlb2 = 'https://eyes-dot-project-path4.appspot.com/countpeople'
+    postData(urlb2,aa)
+    ref.put(aa).then(function(snapshot) {
+      console.log('Uploaded a blob or file!');
+    }).catch(catcher);
+  }).catch(catcher);
+  
+}
+
+function postData(url ='',data = {}){
+  return fetch(url,{
+    method:'POST',
+    mode:'cors',
+    cache:'no-cache',
+    credendials:'same-origin',
+    headers:{
+      'x-api-key': '881a0268-87fa-42a0-a83a-4f70d209636f',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body:data,
+  }).then(res=>res.blob);
+}
+
